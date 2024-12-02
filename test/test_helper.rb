6d492f21e1d7fbb267e04ee7ea8d675761176ed7
Kevin A. <17864518+Kmagameguy@users.noreply.github.com>
2024-12-01 20:28:24 -0500

    Fix Typo

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
-require "mocha/minitests"
+require "mocha/minitest"
 require "minitest/focus"
 
 module ActiveSupport
